ce2b2997c7c28ea9c051c0f1d952540a4a166f0f
VueMid <turabovumid587@gmail.com>
2025-11-24 15:43:09 +0500

    Continue...(11.24.2025/15:44)

---
+++ main.js
@@ -0,0 +1,14 @@
+const menu = document.querySelector(".header__nav-mb");
+const burgerButton = document.querySelector(".header__button-burger");
+const openBurger = document.querySelector(".header__open-burger-icon");
+const closeBurger = document.querySelector(".header__close-burger-icon");
+burgerButton.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  if (menu.classList.contains("active")) {
+    openBurger.style.display = "none";
+    closeBurger.style.display = "block";
+  } else {
+    openBurger.style.display = "block";
+    closeBurger.style.display = "none";
+  }
+});
